f3f60efc3bd909559b19d659a5ade341792624fc
0KN <0knowledgenet@gmail.com>
2023-05-04 11:02:47 -0700

    header bg color fix

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between'>
       <div className='z-10 w-full max-w-6xl items-center justify-between font-mono text-sm lg:flex'>
-        <div className='flex w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
+        <div className='flex w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none'>
           <a
             className='pointer-events-none flex place-items-center gap-2 p-2 lg:pointer-events-auto lg:p-0'
             href='/'
